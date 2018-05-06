94f78e81b7fb72b9a2cf866ba5d82c44637545e2
Pablo <pablo.guardado@live.u-tad.com>
2018-05-06 13:37:31 +0200

    Primer Commit desde intellij

--- src/InterfazGrafica.java
+++ src/InterfazGrafica.java
@@ -7,28 +7,31 @@ public class InterfazGrafica extends Container {
     private JPanel panel1;
     private JButton Hola;
     private JButton Adios;
-    private JTextField textField1;
+    private JTextField TextField1;
 
     public InterfazGrafica() {
         Hola.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textField1.setText("Hola");
+                TextField1.setText("Hola");
             }
         });
         Adios.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textField1.setText("Adios");
+                TextField1.setText("Adios");
             }
         });
     }
     public static void main(String[] args) {
+
         JFrame frame = new JFrame();
         frame.setContentPane(new InterfazGrafica().panel1);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+
     }
 }
 
