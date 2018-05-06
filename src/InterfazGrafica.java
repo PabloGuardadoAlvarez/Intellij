d97e2a2379f6cb4a4a11c697ba87d6c7cb41d913
Pablo <pablo.guardado@live.u-tad.com>
2018-05-06 13:32:08 +0200

    Primer commit desde terminal GitBAsh

---
+++ src/InterfazGrafica.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class InterfazGrafica extends Container {
+    private JPanel panel1;
+    private JButton Hola;
+    private JButton Adios;
+    private JTextField textField1;
+
+    public InterfazGrafica() {
+        Hola.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textField1.setText("Hola");
+            }
+        });
+        Adios.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textField1.setText("Adios");
+            }
+        });
+    }
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setContentPane(new InterfazGrafica().panel1);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
+
+
